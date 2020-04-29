579f54c08fac9c5131edd419be86601f4dc32c54
ttreit <14933277+ttreit@users.noreply.github.com>
2020-04-28 22:18:59 -0700

    Fix formatting

--- src/Grub.js
+++ src/Grub.js
@@ -1,10 +1,10 @@
 class Grub {
-  constructor () {
+  constructor() {
     this.age = 0;
     this.color = 'pink';
     this.food = 'jelly';
   }
-  eat () {
+  eat() {
     return 'Mmmmmmmmm jelly';
   }
 };
